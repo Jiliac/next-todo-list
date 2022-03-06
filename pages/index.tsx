ca17f17ec70877e837655277b59ff422b0f53a95
Valentin Manes <valentinmanes@outlook.fr>
2022-03-06 17:42:22 +0100

    WIP: pause

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,27 @@
+import { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+import { Input } from "../components/input";
+import { List } from "../components/list";
+
 const Home: NextPage = () => {
+  const [list, setList] = useState<Array<string>>([]);
+
+  const onAdd = (todoItem: string) => {
+    setList(prevList => [...list, todoItem])
+  };
+
   return (
     <>
       <h1 className="my-5">My Todo List</h1>
-      <div className="row" style={{ maxWidth: "600px" }}>
-        <div className="col">
-          <input type="text" className="form-control" />
-        </div>
-        <div className="col">
-          <button className="btn btn-primary px-4">Add</button>
-        </div>
-      </div>
+
+      <Input onAdd={onAdd} />
 
       <h3 className="my-4">The List:</h3>
+      <List list={list} />
       <ul style={{ maxWidth: "600px" }}>
         <div className="row">
           <div className="col">
